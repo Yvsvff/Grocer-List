fc3e32f303b6d685e9c015b3a17ff1669b42c7e3
Yusuff Atanda <Yvsvfff@gmail.com>
2023-10-15 12:08:49 -0400

    update

---
+++ grocery-list/app/page.tsx
@@ -0,0 +1,14 @@
+import Content from "@components/Content";
+import Footer from "@components/Footer";
+import Header from "@components/Header";
+
+
+export default function Home() {
+  return (
+    <main > 
+      <Header />
+      <Content />
+      <Footer />  
+       </main>
+  )
+}
